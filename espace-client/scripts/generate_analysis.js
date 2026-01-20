fecc28bd568b670fd6f2371c9a0b9de097d126e9
Bouloxforce <jeremy.heyob@hotmail.com>
2026-01-20 15:58:34 +0100

    Update generate_analysis.js

--- espace-client/scripts/generate_analysis.js
+++ espace-client/scripts/generate_analysis.js
@@ -71,8 +71,21 @@ function getMondayKeyParis(todayYMD) {
 }
 
 function addDaysISO(isoYMD, days) {
+  if (
+    typeof isoYMD !== "string" ||
+    !/^\d{4}-\d{2}-\d{2}$/.test(isoYMD)
+  ) {
+    return null;
+  }
+
   const d = new Date(`${isoYMD}T00:00:00Z`);
+
+  if (Number.isNaN(d.getTime())) {
+    return null;
+  }
+
   d.setUTCDate(d.getUTCDate() + days);
+
   return d.toISOString().slice(0, 10);
 }
 
