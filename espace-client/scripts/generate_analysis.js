af62f912d5c0fae1cdaabb6167c316999f32aba3
Bouloxforce <jeremy.heyob@hotmail.com>
2026-08-06 10:56:16 +0200

    Update generate_analysis.js

--- espace-client/scripts/generate_analysis.js
+++ espace-client/scripts/generate_analysis.js
@@ -198,52 +198,32 @@ function getParisNowISO() {
 
 function logHistoriqueDateChange(data, fieldKey, labelPrefix) {
 
-  data._meta ??= {};
   data.historique ??= [];
 
   const currentValue = data.dates?.[fieldKey];
-  const metaKey = `last_${fieldKey}`;
-  const lastValue = data._meta[metaKey];
 
-  // Pas une vraie date
   if (!isValidISODate(currentValue)) {
-    delete data._meta[metaKey];
     return;
   }
 
-  // Vérifie si l'évènement existe déjà
-  const alreadyExists = data.historique.some(h =>
-    h.label === labelPrefix &&
-    h.date === currentValue
+  const existeDeja = data.historique.some(item =>
+    item.label === labelPrefix &&
+    item.date === currentValue
   );
 
-  // Première saisie
-  if (!lastValue) {
+  if (!existeDeja) {
 
-    if (!alreadyExists) {
-      data.historique.push({
-        date: currentValue,
-        label: labelPrefix
-      });
-    }
+    data.historique.push({
+      date: currentValue,
+      label: labelPrefix
+    });
 
-    data._meta[metaKey] = currentValue;
+    // Tri chronologique
+    data.historique.sort((a, b) => a.date.localeCompare(b.date));
+
   }
 
-  // Modification de la date
-  else if (currentValue !== lastValue) {
-
-    if (!alreadyExists) {
-      data.historique.push({
-        date: currentValue,
-        label: labelPrefix
-      });
-    }
-
-    data._meta[metaKey] = currentValue;
-  }
-
-  // Conserve ton code existant
+  // On conserve la gestion automatique du compromis
   const isoCompromis = getCompromisSignatureISO(data.historique);
 
   if (isoCompromis) {
